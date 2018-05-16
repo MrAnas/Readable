0f6dc606022b5da63fbd89a1a26c30a4b128ec24
Anas <anas2006101@gmail.com>
2018-05-16 16:46:38 +0300

    Structuring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import App from './components/App';
+import thunk from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducer,applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store={this.store}>
+<App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,23 @@
+import {combineReducers} from 'redux'
+
+import {
+    ADD_POST,
+    EDIT_POST,
+    DELETE_POST,
+} from '../actions'
+
+
+function post(state = {} , action){
+    switch(action.type){
+        case ADD_POST:
+            return{
+                
+            }
+    }
+}
+
+
+
+export default combineReducers({
+    // Add here all the reducers
+})
